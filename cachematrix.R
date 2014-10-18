c8506286f339255deb5e15eb58c3cfc74e0012a9
petersedivec@gmail.com <petersedivec@gmail.com>
2014-10-18 12:10:01 -0700

    assignment #2 completed
    includes sample cached vector from assignment and cache matrix work for
    peer review

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains a matrix object that allows for a cached version of the 
+## inverse to be computed and stored so the inverse doesn't need to be computed
+## if the matrix has not changed. Underlying assumption is that all matrices
+## of this type are square and invertable
 
-## Write a short comment describing this function
+## MAKECACHEMATRIX - creates a matrix that contains a cacheable inverse of the
+## matrix so the inverse does not always need to be computed
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    # inv is internal representation of the inverse
+    inv <- NULL # when object is created inverse is set to NULL till computed
+    set <- function(y) { # set/change matrix, as part of this we erase inv value
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x # get the matrix
+    setinv <- function(inverse) inv <<- inverse # assign inverse
+    getinv <- function() inv # return inverse
+    
+    # return a list object with the four functions two gets & two sets
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
+    
 }
 
-
-## Write a short comment describing this function
+## CACHESOLVE - computes the inverse of a CacheMatrix object and commits it to 
+## cache. CACHESOLVE uses SOLVE function to compute the inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    inv <- x$getinv()
+    if (!is.null(inv)) { # test if inv is cached, if so return it
+        message("Returning cached invervse!")
+        return(inv)
+    } else { # inverse is not cached so we need to compute it, cache it, return
+        inv <-solve(x$get())
+        x$setinv(inv)
+        return(inv)
+    }
+        
 }
